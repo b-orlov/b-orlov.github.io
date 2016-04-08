d599e0766993299468d4eed767044bdf91294d17
Bohdan Orlov <bodyaorlov@gmail.com>
2016-04-08 16:54:03 +0300

    Some styling

--- Pokedex/main.js
+++ Pokedex/main.js
@@ -14,21 +14,19 @@ $(document).ready(function() {
 
     function chunk(list) {
         $.get(API + list, function(data) {
-            console.log(data);
             var deck = '';
             for (var i = 0; i < 9; i++) {
                 var type = '';
                 for (var j = 0; j < data.objects[i].types.length; j++){
-                    type += data.objects[i].types[j].name + ' '
+                    type += '<span class="' + data.objects[i].types[j].name + '">' + data.objects[i].types[j].name + '</span> '
                 }
                 NEXT = data.meta.next;
                 PREV = data.meta.previous;
                 deck += '<div class="element" data-uri="'
                       + data.objects[i].resource_uri
                       + '">'
-                      + '<img src="' + API + '/media/img/' + data.objects[i].national_id + '.png' + '" alt="Media image..."/><br>'
-                      + data.objects[i].name
-                      + '<br>'
+                      + '<img src="' + API + '/media/img/' + data.objects[i].national_id + '.png' + '" alt="Media image..."/>'
+                      + '<h4>' + data.objects[i].name + '</h4>'
                       + type
                       + '</div>';
             }
@@ -40,12 +38,24 @@ $(document).ready(function() {
     function click(event) {
         var dataUri = $(event.target).data('uri');
         $.get(API + dataUri, function(result) {
+            console.log(result);
+            var type = '';
+            for (var j = 0; j < result.types.length; j++){
+                type += '<span class="' + result.types[j].name + '">' + result.types[j].name + '</span> '
+            }
             var html =
-                    '<img src="' + API + '/media/img/' + result.national_id + '.png' + '" alt="Media image..."/><br>'
-                    + result.name + ' #' + result.national_id
-                    + '<br>Attack: ' + result.attack
-                    + '<br>Defense:' + result.defense
-            $('.single').html(html);
+                    '<div class="single"><img src="' + API + '/media/img/' + result.national_id + '.png' + '" alt="Media image..."/><br>'
+                    + '<h2>' + result.name + ' #' + result.national_id + '</h2>'
+                    + type
+                    + '<table><tr><td>Attack</td><td>' + result.attack + '</td></tr>'
+                    + '<tr><td>Defense</td><td>' + result.defense + '</td></tr>'
+                    + '<tr><td>HP</td><td>' + result.hp + '</td></tr>'
+                    + '<tr><td>SP Attack</td><td>' + result.sp_atk + '</td></tr>'
+                    + '<tr><td>SP Defense</td><td>' + result.sp_def + '</td></tr>'
+                    + '<tr><td>Speed</td><td>' + result.speed + '</td></tr>'
+                    + '<tr><td>Weight</td><td>' + result.weight + '</td></tr>'
+                    + '<tr><td>Total moves</td><td>' + result.moves.length + '</td></tr></table></div>'
+            $('.poke_wrap').html(html);
         });
     };
 });
