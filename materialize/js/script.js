5f99fcf17556cb29662a7f53ef53aca36e227196
Bohdan Orlov <bodyaorlov@gmail.com>
2016-09-22 00:45:49 +0300

    Initial commit

---
+++ materialize/js/script.js
@@ -0,0 +1,9 @@
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+    
+
+  $('.carousel.carousel-slider').carousel({full_width: true});
